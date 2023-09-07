2597c61fdc6a24415e8405a34bb86bc65e26de63
sreejithliterally <imsreejithofficial@gmail.com>
2023-09-08 00:13:10 +0530

    update

---
+++ quickclean/src/pages/Home.jsx
@@ -0,0 +1,227 @@
+import React, { useState } from 'react';
+import './home.css';
+import bluehero from '../images/bluehero.svg'
+import qc4 from '../images/qc 4.png'
+import qc5 from '../images/qc5.png'
+import qc1 from '../images/qc1.png'
+import qc3 from '../images/qc 3.png'
+import qcog from '../images/qcreal.png'
+import user from '../images/user.jpeg'
+const Home = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div>
+      <header>
+        <a href='#' className='logo'>
+          Quickclean
+        
+        </a>
+
+        <nav className={`navbar ${isMenuOpen ? 'active' : ''}`} style={{ maxHeight: isMenuOpen ? '230px' : '0' }}>
+          <a className="active" href='#home'>Home</a>
+          <a href='#about'>About us</a>
+          <a href='#services'>Services</a>
+          <a href='#contact'>Contact us</a>
+        </nav>
+
+        <div className='icons'>
+          <i className={`fas ${isMenuOpen ? 'fa-times' : 'fa-bars'}`} id="menu-bars" onClick={toggleMenu}></i>
+        </div>
+      </header>
+      <section className='home' id="home">
+        <div className='home-slider'>
+          <div className="wrapper">
+            <div className="slide">
+              <div className="content">
+              <span className='span-text'>Your Trusted Laundry Partner</span>
+                {/* <h3>Quick Clean</h3> */}
+                <div className='hero-image'>
+                <img src={qcog} alt='card' style={{ width: '100%', height: 'auto' }}/>
+                </div>
+                {/* <p>Where Cleanliness Meets Convenience</p> */}
+                <div className="btn-adjust">
+  <a href='#' className='btn' style={{ marginLeft: '14rem' }}>Contact now</a>
+</div>
+
+              </div>
+              <div className='image'>
+  <img src={bluehero} alt="quickclean logo" style={{ width: '100%', height: 'auto' }} />
+</div>
+
+            </div>
+          </div>
+        </div>
+
+      </section>
+
+      <section className='services' id='services'>
+        <h3 className="sub-heading">Our Services</h3>
+        <h1 className="heading">popular services</h1>
+        <div className="box-container">
+          <div className="box">
+            <img src={qc1} alt='card'/>
+            <h3>Steam Ironing</h3>
+            <p>We do Professional Ironing</p>
+            <a href='#' className='btn'>schedule now</a>
+          </div>
+          <div className="box">
+            <img src={qc3} alt='card'/>
+            <h3>Steam Ironing</h3>
+            <p>We do Professional Ironing</p>
+            <a href='#' className='btn'>schedule now</a>
+          </div>
+          <div className="box">
+            <img src={qc1} alt='card'/>
+            <h3>Steam Ironing</h3>
+            <p>We do Professional Ironing</p>
+            <a href='#' className='btn'>schedule now</a>
+          </div>
+          <div className="box">
+            <img src={qc3} alt='card'/>
+            <h3>Steam Ironing</h3>
+            <p>We do Professional Ironing</p>
+            <a href='#' className='btn'>schedule now</a>
+          </div>
+          <div className="box">
+            <img src={qc1} alt='card'/>
+            <h3>Steam Ironing</h3>
+            <p>We do Professional Ironing</p>
+
+            <a href='#' className='btn'>schedule now</a>
+          </div>
+          <div className="box">
+            <img src={qc3} alt='card'/>
+            <h3>Steam Ironing</h3>
+            <p>We do Professional Ironing</p>
+
+            <a href='#' className='btn'>schedule now</a>
+          </div>
+          
+
+        </div>
+      </section>
+
+      <section className='about' id='about'>
+        <h3 className='sub-heading'>about us</h3>
+        <h1 className='heading'>why choose us?</h1>
+        <div className="row">
+          <div className="image">
+            <img src={bluehero} alt='test'/>
+          </div>
+          <div className="content">
+            <h3>best laundary service in kollam</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem in perferendis velit, porro veniam ipsam cupiditate omnis eaque ut. Nemo </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem in perferendis velit, porro veniam ipsam cupiditate omnis eaque ut. Nemo </p>
+            <div className="icons-container">
+              <div className="icons">
+                <i className='fas fa-shipping-fast'></i>
+                <span>Fast process</span>
+              </div>
+              <div className="icons">
+                <i className='fas fa-headset'></i>
+                <span>customer friendly</span>
+              </div>
+              <div className="icons">
+                <i className='fas fa-dollar-sign'></i>
+                <span>low cost</span>
+              </div>
+            </div>
+            <a href='#' className='btn'>know more</a>
+
+          </div>
+        </div>
+      </section>
+
+
+      <section className='review' id='review'>
+        <h3 className='sub-heading'>customer's review</h3>
+        <h1 className='heading'>what they say</h1>
+
+        <div className="swiper-container review-slider">
+          <div className="swiper-wrapper">
+            <div className="swiper-slide slide">
+              <i className='fas fa-quote-right'></i>
+              <div className="user">
+              <img src={user} alt=''/>
+              <div className="user-info">
+                <h3>xoom</h3>
+                <div className="stars">
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star-half-alt'></i>
+                </div>
+              </div>
+              </div>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus voluptate nisi quam fugiat temporibus blanditiis deserunt atque dolorum consequatur aliquid, magni animi</p>
+            </div>
+
+            <div className="swiper-slide slide">
+              <i className='fas fa-quote-right'></i>
+              <div className="user">
+              <img src={user} alt=''/>
+              <div className="user-info">
+                <h3>xoom</h3>
+                <div className="stars">
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star-half-alt'></i>
+                </div>
+              </div>
+              </div>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus voluptate nisi quam fugiat temporibus blanditiis deserunt atque dolorum consequatur aliquid, magni animi</p>
+            </div>
+
+
+            <div className="swiper-slide slide">
+              <i className='fas fa-quote-right'></i>
+              <div className="user">
+              <img src={user} alt=''/>
+              <div className="user-info">
+                <h3>xoom</h3>
+                <div className="stars">
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star-half-alt'></i>
+                </div>
+              </div>
+              </div>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus voluptate nisi quam fugiat temporibus blanditiis deserunt atque dolorum consequatur aliquid, magni animi</p>
+            </div>
+
+
+            <div className="swiper-slide slide">
+              <i className='fas fa-quote-right'></i>
+              <div className="user">
+              <img src={user} alt=''/>
+              <div className="user-info">
+                <h3>xoom</h3>
+                <div className="stars">
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star'></i>
+                  <i className='fas fa-star-half-alt'></i>
+                </div>
+              </div>
+              </div>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Delectus voluptate nisi quam fugiat temporibus blanditiis deserunt atque dolorum consequatur aliquid, magni animi</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
